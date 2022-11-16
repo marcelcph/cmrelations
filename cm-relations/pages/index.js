935479a11653f63e24e580c3d8ac0981b0a5ac2a
emilmarcel <55388306+marcelcph@users.noreply.github.com>
2022-11-16 17:32:52 +0100

    Tailwind setup

--- cm-relations/pages/index.js
+++ cm-relations/pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 class="bg-gray-300">test af tailwind</h1>
+        <h1>test af tailwind</h1>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
